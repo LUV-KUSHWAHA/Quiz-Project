2fca3083e7cbf348b20489bdd1b4a4441489b0e2
Luv Kushwaha <santosh.079bct059@acem.edu.np>
2025-06-02 00:02:53 +0545

    question added

--- game.py
+++ game.py
@@ -4,13 +4,15 @@ questions = ("How many elements are there in the periodic table ?",
              "Which animal lays the largest eggs ?",
              "what is the most abundant gas in Earth's atmosphere ?",
              "How many bones are there in the human body ?",
-             "Which planet in the solar system is the hottest ?")
+             "Which planet in the solar system is the hottest ?",
+             "What is the value of acceleration due to gravity ?")
 options = (("A. 116","B. 117","C. 118","D. 119"),
            ("A. Whale","B. Crocodile","C. Elephant","D. Ostrich"),
            ("A. Nitrogen","B. Oxygen","C. Carbon dioxide","D. Hydrogen"),
            ("A. 206","B. 207","C. 208","D. 209"),
-           ("A. Mercury","B. Venus","C.Earth","D. Mars"))
-answers =("C","D","A","A","B")
+           ("A. Mercury","B. Venus","C.Earth","D. Mars"),
+           ("A. 9.6m/s2", "B. 9.7m/s2", "C. 9.8m/s2", "D. 9.9m/s2"))
+answers =("C","D","A","A","B","C")
 guesses = []
 score = 0
 question_num = 0
